7720096a70cab56d6e17be191469f8d548393e2a
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-07 04:41:34 -0700

    Changed the name from 'main.h' to 'shell.h'

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Defined vars */
+#define PROMPT "$ "
+#define DELIMETER " \n"
+
+/* The imported libraries */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
