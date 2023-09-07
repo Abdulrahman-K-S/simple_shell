5f7e9ccba781bb2a8a0e6a4ece961230743c5e4f
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-07 05:07:12 -0700

    Starting point

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * main - The starting point for the shell project
@@ -6,9 +6,31 @@
  * @ac: Number of comandline arguments.
  * @argv: Pointer to an array of command line arguments.
  *
- * Return: 1
+ * Return: 1 indicating a success. -1 if there was an error.
 */
 int main(int ac, char **argv)
 {
+    char *lineptr;
+    size_t n = 0;
+    ssize_t n_read;
+
+    (void)ac; (void)argv;
+
+    while (1)
+    {
+        /* Print off the prompt before the user input */
+        _puts(PROMPT);
+
+        /* Check if getline failed or CTRL+D was pressed */
+        n_read = getline(&lineptr, &n, stdin);
+        if (n_read == -1)
+            return (-1);
+
+        _puts(lineptr);
+
+        /* Frees the pointer after the operations are done for reuse */
+        free(lineptr);
+    }
+
 	return (1);
 }
