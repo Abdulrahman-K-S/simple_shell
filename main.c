30f87a80ef9d5d3fbbab468faf3144cd16d747f4
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-07 03:37:14 -0700

    Added 'main.c' The first step for the shell project. Fullfills the requirments for '0. Betty would be proud'

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - The starting point for the shell project
+ *
+ * @ac: Number of comandline arguments.
+ * @argv: Pointer to an array of command line arguments.
+ *
+ * Return: 1
+*/
+int main(int ac, char **argv)
+{
+	return (1);
+}
