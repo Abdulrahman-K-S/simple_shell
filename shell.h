9be440b52a14452f6f04a1a182fdd6c511c49631
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-12 05:15:56 -0700

    Added prototype Added the int getNumberofTokens(const char *lineptr) prototype to the header file

--- shell.h
+++ shell.h
@@ -11,8 +11,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* printers.c Modules */
+
 /* The printers for the shell */
 void _putchar(char c);
 void _puts(char *s);
+
+/* input_parsing.c Modules */
+
+/* The input manipulators so the shell can understand */
+/*   what the user is typing */
+int getNumberofTokens(const char *lineptr);
 
 #endif
