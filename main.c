90d1352b365839c615871a45359565511866e4e2
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-13 05:59:16 -0700

    Fixed the betty style

--- main.c
+++ main.c
@@ -10,28 +10,31 @@
 */
 int main(int ac, char **argv)
 {
-    char *lineptr;
-    size_t n = 0;
-    int numOFtokens;
+	char *lineptr;
+	size_t n = 0;
+	int numOFtokens;
 
-    (void)ac; (void)argv;
+	(void)ac;
+	(void)argv;
 
-    while (1)
-    {
-        /* Print off the prompt before the user input */
-        _puts(PROMPT);
+	while (1)
+	{
+		/* Print off the prompt before the user input */
+		_puts(PROMPT);
 
-        /* Check if getline failed or CTRL+D was pressed */
-        if (getline(&lineptr, &n, stdin) == -1)
-            return (-1);
+		/* Check if getline failed or CTRL+D was pressed */
+		if (getline(&lineptr, &n, stdin) == -1)
+			return (-1);
 
-	numOFtokens = getNumberofTokens(lineptr);
+		numOFtokens = getNumberofTokens(lineptr);
 
-        _puts(lineptr);
+		printf("The number of words got are: %d\n", numOFtokens);
 
-        /* Frees the pointer after the operations are done for reuse */
-        free(lineptr);
-    }
+		_puts(lineptr);
+
+		/* Frees the pointer after the operations are done for reuse */
+		free(lineptr);
+	}
 
 	return (1);
 }
