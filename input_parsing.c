0f0b7ec89c9379a65691fd43e4e1f11759e58c47
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-14 05:43:32 -0700

    Updated 'input_parsing.c' Added 'storeTokens' function that stores the command input broken down in argv variable

--- input_parsing.c
+++ input_parsing.c
@@ -8,7 +8,6 @@
  *
  * Return: The number of words/tokens in @lineptr.
 */
-
 int getNumberofTokens(char *lineptr)
 {
 	int num_of_tokens = 0;
@@ -26,4 +25,32 @@ int getNumberofTokens(char *lineptr)
 	num_of_tokens++;
 
 	return (num_of_tokens);
+}
+
+/**
+ * storeTokens - A function that takes the inputed command and
+ *               breaks it down to tokens and place those tokens into
+ *               the argument pointer.
+ *
+ * @argv: The argument pointer.
+ * @lineptr: The command the user inputed.
+*/
+void storeTokens(char **argv, char *lineptr)
+{
+	int i;
+	char *token, *lineptr_cpy = malloc(sizeof(char) * _strlen(lineptr));
+
+	_strcpy(lineptr_cpy, lineptr);
+
+	token = strtok(lineptr_copy, DELIMETER);
+
+	for (i = 0; token != NULL; i++)
+	{
+		argv[i] = malloc(sizeof(char) * _strlen(token));
+		_strcpy(argv[i], token);
+
+		token = strtok(NULL, DELIMETER);
+	}
+
+	argv[i] = NULL;
 }
