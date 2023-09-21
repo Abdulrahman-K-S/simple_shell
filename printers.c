f790ce75e2bcdcb84286c9fd7baa8121dea21b6e
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-21 06:30:52 -0700

    Revamped 'printers.c'
    Cleaned them up and added a buffer

--- printers.c
+++ printers.c
@@ -1,24 +1,43 @@
 #include "shell.h"
 
 /**
- * _puts - prints a string
+ * _putchar - A function that writes the character to stdout.
  *
- * @s: string input
+ * @Character: The character to be printed.
+ *
+ * Return: On sucess 1.
 */
-
-void _puts(char *s)
+int _putchar(char Character)
 {
-	while (*s)
-		_putchar(*s++);
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (Character == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (Character != BUF_FLUSH)
+		buf[i++] = Character;
+
+	return (1);
 }
 
 /**
- * _putchar - A function that writes out a character.
+ * _puts - A function that prints an input string.
  *
- * @c: A character entered to write to std.
+ * @String: The string to be printed.
 */
-
-void _putchar(char c)
+void _puts(char *String)
 {
-	write(1, &c, 1);
+	int i = 0;
+
+	if (!String)
+		return;
+
+	while (String[i] != '\0')
+	{
+		_putchar(String[i]);
+		i++;
+	}
 }
