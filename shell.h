c7cea91deb42e1b4fee865fa32cd0ffc43be9735
fadirafik <fadihadddad11@gmail.com>
2023-09-22 12:32:08 +0300

    modified shell.h

--- shell.h
+++ shell.h
@@ -106,6 +106,11 @@ typedef struct passinfo
 	int histcount;
 } info_t;
 
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+
 /* ==================== */
 /* memorytime.c Modules */
 /* ==================== */
