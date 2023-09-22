cc3b280fcf64492f7866e4a1129181c06c2ea9b7
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-21 18:01:26 -0700

    Added 'error_functions.c'
    This file houses the printers for the errors

---
+++ error_functions.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints an input string
+ *
+ * @String: the string to be printed
+*/
+void _eputs(char *String)
+{
+	int i = 0;
+
+	if (!String)
+		return;
+
+	while (String[i] != '\0')
+	{
+		_eputchar(String[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - writes the character c to stderr
+ *
+ * @Character: The character to be print
+ *
+ * Return: On success 1.
+*/
+int _eputchar(char Character)
+{
+	static int i;
+	static char buffer[WRITE_BUF_SIZE];
+
+	if (Character == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buffer, i);
+		i = 0;
+	}
+	if (Character != BUF_FLUSH)
+		buffer[i++] = Character;
+
+	return (1);
+}
+
+/**
+ * _putsfd - prints an input string.
+ *
+ * @String: the string to be printed
+ * @File: the file descriptor to write to
+ *
+ * Return: the number of chars put
+*/
+int _putsfd(char *String, int File)
+{
+	int i = 0;
+
+	if (!String)
+		return (0);
+	while (*String)
+	{
+		i += _putfd(*String++, File);
+	}
+	return (i);
+}
+
+/**
+ * _putfd - writes the character to a given file descriptor.
+ *
+ * @Character: The character to be print.
+ * @File: The file descriptor to write to.
+ *
+ * Return: On success 1.
+*/
+int _putfd(char Character, int File)
+{
+	static int i;
+	static char buffer[WRITE_BUF_SIZE];
+
+	if (Character == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(File, buffer, i);
+		i = 0;
+	}
+	if (Character != BUF_FLUSH)
+		buffer[i++] = Character;
+
+	return (1);
+}
