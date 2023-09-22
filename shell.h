12113b1b94a54b101bfe919c1ddbd5abba18e480
fadirafik <fadihadddad11@gmail.com>
2023-09-22 12:30:43 +0300

    added new struct info_t

--- shell.h
+++ shell.h
@@ -61,6 +61,51 @@ size_t list_len(const list_t *);
 ssize_t get_node_index(list_t *, list_t *);
 void free_list(list_t **);
 
+/**
+ * struct passinfo - contains pseudo-arguements to pass into a function,
+ *					allowing uniform prototype for function pointer struct
+ * @arg: a string generated from getline containing arguements
+ * @argv: an array of strings generated from arg
+ * @path: a string path for the current command
+ * @argc: the argument count
+ * @line_count: the error count
+ * @err_num: the error code for exit()s
+ * @linecount_flag: if on count this line of input
+ * @fname: the program filename
+ * @env: linked list local copy of environ
+ * @environ: custom modified copy of environ from LL env
+ * @history: the history node
+ * @alias: the alias node
+ * @env_changed: on if environ was changed
+ * @status: the return status of the last exec'd command
+ * @cmd_buf: address of pointer to cmd_buf, on if chaining
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @readfd: the fd from which to read line input
+ * @histcount: the history line number count
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char *cmd_buf; /*pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
+} info_t;
+
 /* ==================== */
 /* memorytime.c Modules */
 /* ==================== */
@@ -75,9 +120,9 @@ void *_realloc(void *, unsigned int, unsigned int);
 /* =========================== */
 
 void _eputs(char *);
-int _eputchar(char );
+int _eputchar(char);
 int _putsfd(char *, int);
-int _putfd(char , int);
+int _putfd(char, int);
 
 /* ==================== */
 /*  printers.c Modules  */
